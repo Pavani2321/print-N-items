93012e328eaf1c25753487ddef3868d289423886
Pavani <124267666+Pavani2321@users.noreply.github.com>
2023-03-02 10:19:32 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+number=int(input())
+counter = 0
+while counter<number:
+    print("0")
+    counter = counter + 1
+ 
+#Approah by using for loop
+
+#number=int(input())
+#for i in range(number):
+    #print("0")
+    
+    
+    
